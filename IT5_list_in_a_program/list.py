48836337c6e103006328b754ed5ff5725a781c42
John Basile <ashton.basile@gmail.com>
2023-07-14 13:51:44 +0100

    I made a list of teams (dictionaries), where the first item in the list provides the index in the list where each team can be found.

---
+++ IT5_list_in_a_program/list.py
@@ -0,0 +1,29 @@
+# Demonstrate the use of a list in a program. Include in your screenshot(s):
+# *A list in a program
+# *A function (that you’ve created) that uses the list
+# *The result of the function running
+# Submit a comment explaining your understanding of:
+# *The use of lists in programs
+# *The code you’ve submitted
+
+teams = [
+	{
+		"Accounts Team": 1,
+		"Art Team": 2,
+		"Facilities Team": 3
+	},
+	{
+		"rob": "banker",
+		"steve": "salesman",
+		"angela": "accountant",
+		"gwen": "banker"
+	},
+	{
+		"ariel": "art director",
+		"miriam": "painter"
+	},
+	{
+		"javier": "maintenance member",
+		"david": "grounds crew member"
+	}
+]
