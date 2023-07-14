55cb90a6f80a2f05969fb58fbcfd79ec99a5acda
John Basile <ashton.basile@gmail.com>
2023-07-14 14:23:29 +0100

    I made a dictionary containing info about a type of dragon called a tatzelwurm

---
+++ IT6_dictionary_in_a_program/dictionary.py
@@ -0,0 +1,13 @@
+# Demonstrate the use of a dictionary in a program. 
+# Include in your screenshot(s):
+# *A dictionary in a program
+# *A function (that you’ve created) that uses the dictionary
+# *The result of the function running
+
+tatzelwurm = {
+	"other-names":["stollenwurm","stollwurm"],
+	"region":"alps",
+	"creature-type":"lizard-like",
+	"face-style":"cat-like",
+	"body-type":"serpent-like",
+}
