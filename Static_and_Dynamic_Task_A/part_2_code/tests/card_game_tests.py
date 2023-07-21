51cab6ebea3f7aed295bfc79a0935470308292fe
John <ashton.basile@gmail.com>
2023-07-21 13:40:48 +0100

    I wrote the tests first in a test-driven development fashion.

--- Static_and_Dynamic_Task_A/part_2_code/tests/card_game_tests.py
+++ Static_and_Dynamic_Task_A/part_2_code/tests/card_game_tests.py
@@ -2,5 +2,21 @@ import unittest
 from src.card import Card
 from src.card_game import CardGame
 
-class TestCardGame:
-    pass
+class TestCardGame(unittest.TestCase):
+    def setUp(self):
+        self.king_of_hearts = Card("hearts", 10)
+        self.queen_of_clubs = Card("clubs", 10)
+        self.two_of_diamonds = Card("diamonds", 2)
+        self.ace_of_spades = Card("spades", 1)
+        self.cards = [self.king_of_hearts, self.queen_of_clubs, self.two_of_diamonds, self.ace_of_spades]
+
+    def test_check_for_ace_function(self):
+        self.assertEqual(True, CardGame.check_for_ace(self, self.king_of_hearts))
+        self.assertEqual(False, CardGame.check_for_ace(self, self.ace_of_spades))
+
+    def test_highest_card_function(self):
+        self.assertEqual(self.two_of_diamonds, CardGame.highest_card(self, self.king_of_hearts, self.two_of_diamonds))
+        self.assertEqual(self.two_of_diamonds, CardGame.highest_card(self, self.queen_of_clubs, self.two_of_diamonds))
+
+    def test_cards_total_function(self):
+        self.assertEqual("You have a total of5", CardGame.cards_total(self, self.cards))
