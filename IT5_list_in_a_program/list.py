d883366aacb8cfdc8a1843c3496700f1ccf7ba3a
John <ashton.basile@gmail.com>
2023-07-21 11:48:07 +0100

    I added more information explaining my understanding of lists, dictionaries, classes, and objects.

--- IT5_list_in_a_program/list.py
+++ IT5_list_in_a_program/list.py
@@ -49,3 +49,27 @@ def find_job_title(teams_list, team_name, name):
 	return job_title
 
 print(find_job_title(teams, "Facilities Team", "David"))
+
+# further explanation of the code to meet the submission requirements:
+# The find_job_title function that I wrote finds the job title from the 
+# list by first calling the other function that I wrote which is called 
+# find_team_index. The find_team_index function returns the index of the 
+# team that the faculty member we are looking for is assigned to. 
+# The teams list is a list of dictionary objects. 
+# The first dictionary object in the list is a directory telling us
+#  where in the list we can find the particular team we are looking for. 
+# The other items in the last are dictionary objects  representing the 
+# individual teams. Each team contains the employees that work on that 
+# particular team along with their job titles stored as values. 
+# The find_team_index function searches the first element in the list 
+# to obtain the index for the team_name that is passed in. 
+# The find_job_title function then takes that index and uses it 
+# find the dictionary object in the list that represents the team 
+# we are looking for. Then, inside of that dictionary item, it 
+# finds the name of the worker and returns their job title.
+
+# Basically, lists allow us to store items in an ordered way. 
+# We can then access items in the list if we know the index 
+# (or order) in which they exist in the list. We can index 
+# into a list by using square brackets and putting the index 
+# inside of the square brackets.
--- IT6_dictionary_in_a_program/dictionary.py
+++ IT6_dictionary_in_a_program/dictionary.py
@@ -4,6 +4,11 @@
 # *A function (that you’ve created) that uses the dictionary
 # *The result of the function running
 
+# Dictionaries can be used to store key, value pairs. 
+# They are useful for saving information that comes in pairs. 
+# I have implemented a function that accesses a dictionary that 
+# I made to get information about a particular type of dragon called the tatzelwurm.
+
 tatzelwurm = {
 	"other-names":["stollenwurm","stollwurm"],
 	"region":"alps",
@@ -16,3 +21,10 @@ def get_element(dict, key):
 	return dict[key]
 
 print(f"The region of the tatzelwurm is {get_element(tatzelwurm, 'region')}")
+
+# More Explanation
+# I wrote the get_element function (the simplest implementation of a dictionary access). 
+# It allows me to get the value of an item in the dictionary 
+# simply be passing in the dictionary and the key. 
+# The get_element function works by using square brackets to access 
+# the value of the key that is passed in. A dictionary works by storing key, value pairs.
